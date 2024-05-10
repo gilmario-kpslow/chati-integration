8f5aee717bafe6d9bb4e8bcdbc6a264a6be7e889
Gilmario <gilmariosoftware@gmail.com>
2024-05-10 11:41:53 -0300

    Comit automatico gerado pelo sistema

--- src/main/java/br/gov/ce/sefaz/chati/versao/VersaoResource.java
+++ src/main/java/br/gov/ce/sefaz/chati/versao/VersaoResource.java
@@ -3,7 +3,11 @@ package br.gov.ce.sefaz.chati.versao;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 import java.util.ResourceBundle;
 
 /**
@@ -23,4 +27,30 @@ public class VersaoResource {
                 .versao(bundle.getString("VERSAO"))
                 .projeto(bundle.getString("PROJETO")).build();
     }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("exemplo")
+    public void exemplo(@QueryParam("comando") String comando) throws Exception {
+
+        int porta = 9001;
+        String servidor = "localhost";
+        InetSocketAddress adress = new InetSocketAddress(servidor, porta);
+
+        SocketChannel cliente = SocketChannel.open();
+
+        cliente.connect(adress);
+
+        ByteBuffer b = ByteBuffer.wrap(comando.getBytes());
+
+        int num = cliente.write(b);
+
+        System.out.println(num);
+
+//        byte[] resp = cliente.socket().getInputStream().readAllBytes();
+//        System.out.println(new String(resp));
+        cliente.finishConnect();
+
+    }
+
 }
